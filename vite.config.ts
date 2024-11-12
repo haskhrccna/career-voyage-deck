e93fcca83c86ff739d6fe7aa42b3d2410e9801d7
haskhrccna <haskhrccna@gmail.com>
2024-11-12 15:37:42 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  root: process.cwd(), // Explicitly set root to current working directory
+  root: '.', // Keep this simple
+  base: '/',
   server: {
     host: "::",
     port: 8080,
@@ -22,11 +22,7 @@ export default defineConfig(({ mode }) => ({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    assetsDir: 'assets',
     sourcemap: true,
-    rollupOptions: {
-      input: {
-        app: path.resolve(process.cwd(), 'index.html'),
-      },
-    },
   },
 }));
