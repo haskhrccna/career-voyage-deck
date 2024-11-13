eee5b37f805472bff64a85fad03e59736e4748fd
Hassan Adam <haskhr@hotmail.com>
2024-11-13 23:32:00 +0400

    edit from claude 13

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,28 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-
 export default defineConfig({
   plugins: [react()],
-  root: process.cwd(),
-  base: '', // Empty base to use relative paths
+  base: '/',  // Change from './' to '/'
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    manifest: true,
-    modulePreload: {
-      polyfill: false
-    },
+    assetsDir: 'assets',
+    cssCodeSplit: false,
     rollupOptions: {
-      input: './index.html',
-      preserveEntrySignatures: "strict",
       output: {
-        format: 'es',
-        entryFileNames: '[name].[hash].js',
-        chunkFileNames: '[name].[hash].js',
-        assetFileNames: '[name].[hash][extname]'
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.')
+          const ext = info[info.length - 1]
+          if (/\.(css|less|sass|scss|styl|stylus|pcss|postcss)$/i.test(assetInfo.name)) {
+            return `assets/css/[name].[hash][extname]`
+          }
+          if (/\.(png|jpe?g|gif|svg|ico|webp)$/i.test(assetInfo.name)) {
+            return `assets/images/[name].[hash][extname]`
+          }
+          return `assets/[ext]/[name].[hash][extname]`
+        },
+        chunkFileNames: 'assets/js/[name].[hash].js',
+        entryFileNames: 'assets/js/[name].[hash].js'
       }
-    },
-    assetsInlineLimit: 0,
-    cssCodeSplit: true,
-    sourcemap: false
+    }
   },
   resolve: {
     alias: {
