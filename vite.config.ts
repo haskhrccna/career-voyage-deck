e4e6c35912a2025bd9c8aa97aaf7e18c344d8686
haskhrccna <haskhrccna@gmail.com>
2024-11-13 17:15:22 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,34 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  root: '.', // Keep this simple
-  //root: 'dist', // If `index.html` is located in `/app/dist/`
-  base: '/',
-  server: {
-    host: "::",
-    port: 8080,
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
   },
-  plugins: [
-    react(),
-    mode === 'development' && componentTagger(),
-  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: 'index.html',
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-      }
-    }
   }
-}));
+});
