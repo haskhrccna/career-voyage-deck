0f5aad39183a351b683a7b3c8c6d390565a0bb34
haskhrccna <haskhrccna@gmail.com>
2024-12-07 23:20:16 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css' // This assumes you have a global CSS file - we can create one if needed
 
+// Ensure the root element exists in your HTML
+const rootElement = document.getElementById('root')
+
+if (!rootElement) {
+  throw new Error(
+    'Root element not found. Please add a <div id="root"></div> to your HTML'
+  )
+}
+
+// Create a root and render the app
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
